f5a78a10dcd1736258d803b5d48dd1202dc87ba9
Sébastien Gatineau <sebastien.gatineau@outlook.com>
2024-10-21 09:31:26 +0200

    wip: add create weekly menu

--- menu/menu.go
+++ menu/menu.go
@@ -1,1 +1,21 @@
 package menu
+
+import "menuForge/meal"
+
+type DayMenu struct {
+	lunch  meal.Meal
+	dinner meal.Meal
+}
+
+type WeekMenu struct {
+	monday DayMenu
+}
+
+func (weekMenu *WeekMenu) Monday() DayMenu {
+	return weekMenu.monday
+}
+
+func CreateWeekMenu(meals []meal.Meal) WeekMenu {
+
+	return WeekMenu{}
+}
--- menu/menu_test.go
+++ menu/menu_test.go
@@ -1,9 +1,38 @@
 package menu
 
 import (
+	"menuForge/meal"
 	"testing"
 )
 
-func TestCreateMenu(t *testing.T) {
+func TestCreateWeeklyMenu_AsDailyMenuPerDay(t *testing.T) {
+	meals := []meal.Meal{
+		meal.New("Pasta with meatball", true),
+		meal.New("Quinoa with butternut", false),
+		meal.New("Grilled chicken with vegetables", true),
+		meal.New("Vegetable stir-fry", false),
+		meal.New("Beef stew", true),
+		meal.New("Lentil soup", false),
+		meal.New("Chicken curry", true),
+		meal.New("Vegetable curry", false),
+		meal.New("Pork chops", true),
+		meal.New("Mushroom risotto", false),
+		meal.New("Fish tacos", true),
+		meal.New("Vegetable tacos", false),
+		meal.New("Lamb kebabs", true),
+		meal.New("Falafel wrap", false),
+		meal.New("Turkey sandwich", true),
+		meal.New("Caprese salad", false),
+		meal.New("Beef burger", true),
+		meal.New("Veggie burger", false),
+		meal.New("Chicken salad", true),
+		meal.New("Greek salad", false),
+	}
 
+	weekMenu := CreateWeekMenu(meals)
+
+	mondayMenu := weekMenu.Monday()
+	if mondayMenu.lunch.Name != "" {
+
+	}
 }
