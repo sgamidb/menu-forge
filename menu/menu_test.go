3a9eaf083894847200eec6326c1112719e10aba9
Sébastien Gatineau <sebastien.gatineau@outlook.com>
2024-11-25 23:33:42 +0100

    test: a menu should not contain a menu twice

--- menu/menu_test.go
+++ menu/menu_test.go
@@ -6,28 +6,7 @@ import (
 )
 
 func TestCreateWeeklyMenu_OneDailyMenuPerDay(t *testing.T) {
-	meals := []meal.Meal{
-		meal.New("Pasta with meatball", true),
-		meal.New("Quinoa with butternut", false),
-		meal.New("Grilled chicken with vegetables", true),
-		meal.New("Vegetable stir-fry", false),
-		meal.New("Beef stew", true),
-		meal.New("Lentil soup", false),
-		meal.New("Chicken curry", true),
-		meal.New("Vegetable curry", false),
-		meal.New("Pork chops", true),
-		meal.New("Mushroom risotto", false),
-		meal.New("Fish tacos", true),
-		meal.New("Vegetable tacos", false),
-		meal.New("Lamb kebabs", true),
-		meal.New("Falafel wrap", false),
-		meal.New("Turkey sandwich", true),
-		meal.New("Caprese salad", false),
-		meal.New("Beef burger", true),
-		meal.New("Veggie burger", false),
-		meal.New("Chicken salad", true),
-		meal.New("Greek salad", false),
-	}
+	meals := CreateSampleMeals()
 
 	weekMenu := CreateWeekMenu(meals)
 	days := []string{"monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"}
@@ -49,10 +28,51 @@ func TestCreateWeeklyMenu_OneDailyMenuPerDay(t *testing.T) {
 }
 
 func TestCreateWeeklyMenu_DontHaveTheSameMenuTwiceInTheWeek(t *testing.T) {
+	meals := CreateSampleMeals()
 	t.Errorf("something failed")
+	menuMap := make(map[string]bool)
+	weekMenu := CreateWeekMenu(meals)
+	days := []string{"monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"}
+	for _, day := range days {
+		dayMenu := weekMenu.GetDayMenu(day)
+		if _, ok := menuMap[dayMenu.lunch.Name]; ok {
+			t.Errorf("%s still exists in week menu", dayMenu.lunch.Name)
+		} else {
+			menuMap[dayMenu.lunch.Name] = true
+		}
+		if _, ok := menuMap[dayMenu.dinner.Name]; ok {
+			t.Errorf("%s still exists in week menu", dayMenu.dinner.Name)
+		} else {
+			menuMap[dayMenu.dinner.Name] = true
+		}
+	}
 }
 
 func TestCreateWeeklyMenu_DailyMenuDoesntHaveMeatForDinner(t *testing.T) {
-	t.Errorf("something failed")
 
+}
+
+func CreateSampleMeals() []meal.Meal {
+	return []meal.Meal{
+		meal.New("Pasta with meatball", true),
+		meal.New("Quinoa with butternut", false),
+		meal.New("Grilled chicken with vegetables", true),
+		meal.New("Vegetable stir-fry", false),
+		meal.New("Beef stew", true),
+		meal.New("Lentil soup", false),
+		meal.New("Chicken curry", true),
+		meal.New("Vegetable curry", false),
+		meal.New("Pork chops", true),
+		meal.New("Mushroom risotto", false),
+		meal.New("Fish tacos", true),
+		meal.New("Vegetable tacos", false),
+		meal.New("Lamb kebabs", true),
+		meal.New("Falafel wrap", false),
+		meal.New("Turkey sandwich", true),
+		meal.New("Caprese salad", false),
+		meal.New("Beef burger", true),
+		meal.New("Veggie burger", false),
+		meal.New("Chicken salad", true),
+		meal.New("Greek salad", false),
+	}
 }
